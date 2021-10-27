ddc18a275b19d4334248d5b1cab8a41ccf90b5a1
ttaranto <tom.taranto@yahoo.fr>
2021-10-27 22:28:48 +0200

    premieres implems de agent, ajouts dans actifs

--- actif.py
+++ actif.py
@@ -1,32 +1,30 @@
 from typing import Dict
 
+
 class Actif():
     # history = [date : [buyer : (quantity, price)]]
-    def __init__(self, name: str, quantity : float, price : float, history: Dict[str,Dict[str, (float,float)]]):
+    def __init__(self, name: str, quantity: float, price: float, history: Dict[str, Dict[str, (float, float)]]):
         self.name = name
         self.quantity = quantity
         self.price = price
         self.history = history
 
-    def get_price(self)->float:
+    def get_price(self) -> float:
         return self.price
 
-
-    #Achat avec quantity positif
-    #Vente avec quantity negatif
+    # Achat avec quantity positif
+    # Vente avec quantity negatif
     # Obligatoirement appeller can_buy() avant d'appeller cette fonction
-    def update(self, quantity: float, date: str, buyer: str):
+    def update(self, quantity: float, date: str, buyer: str)->None:
         self.quantity -= quantity
         self.history[date[buyer]] = (quantity, self.price)
 
-
-
-    def set_price(self, price: float):
+    def set_price(self, price: float) -> None:
         self.price = price
 
     # Obligatoirement appeller cette fonction avant d'appeller update
-    def can_buy(self, quantity: float)->bool:
-        if self.quantity> quantity:
+    def can_buy(self, quantity: float) -> bool:
+        if self.quantity > quantity:
             return True
         return False
 
--- agent.py
+++ agent.py
@@ -1,2 +1,13 @@
+from compte import Compte
+
+
 class Agent():
-    pass
+    def __init__(self, name: str, agent_type: str, compte: Compte):
+        assert agent_type in ["human", "bot"]
+        self.name = name
+        self.agent_type = agent_type
+        self.compte = compte
+        self.age = 0
+
+
+
